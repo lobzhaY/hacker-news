b7defc362db9c87a1be5c891a9e047c6b5f3a417
Yuliya Lobzha <yulia.lobzha@gmail.com>
2024-10-23 21:34:48 +0300

    add not-found page

---
+++ src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,12 @@
+import { FrownOutlined } from "@ant-design/icons"
+
+export const NotFoundPage: React.FC = () => {
+    return (
+        <div>
+            <div>
+                4<FrownOutlined />4
+            </div>
+            <h2>Page not found</h2>
+        </div>
+    )
+}
